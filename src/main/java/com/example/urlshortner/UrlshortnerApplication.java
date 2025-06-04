aa1eda0929674aa1caf622517b0904c290fa9a4b
deevyanshu <devyanshugarg2000@gmail.com>
2025-06-04 21:46:46 +0530

    Update UrlshortnerApplication.java

--- src/main/java/com/example/urlshortner/UrlshortnerApplication.java
+++ src/main/java/com/example/urlshortner/UrlshortnerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://glittery-strudel-69e8b1.netlify.app")
 @SpringBootApplication
 public class UrlshortnerApplication {
 
